9d36f9516268669020ee89553bf1715856ebb3df
FEITAN <guisilva4410@gmail.com>
2025-10-22 23:53:17 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const toggleMenu = document.querySelector('.btn-toggle-menu');
+const btnToggle = document.querySelector('.btn-toggle-menu');
 const menu = document.querySelector('.menu');
 
-toggleMenu.addEventListener('click', () => {
+btnToggle.addEventListener('click', () => {
   menu.classList.toggle('ativo');
 });
